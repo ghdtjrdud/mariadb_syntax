eb3487fceb8bf13a890900b2c0f28e260824b5d9
ghdtjrdud <ghdtjrdud38@gmail>
2024-11-21 14:04:33 +0900

    sql having 절

--- 8.join.sql
+++ 8.join.sql
@@ -56,3 +56,46 @@ where id in (select count(*) from post where author_id=a.id);
 -- from 절
 select a.name
 from (select * from author)as a;
+
+-- 집계함수
+-- null은 count에서 제외
+select count(*) from author;
+select sum(*) from author;
+select avg(*) from author;
+
+-- 소수점 첫번째자리에서 반올림해서 소수점을 없앰.(0번째 자리로 만들기)
+select round(avg(price), 0) from post;
+
+-- group by : 그룹화된 데이터를 데이터 별로 취급.
+-- author_id로 그룹핑 하였으면, 그 외의 컬럼을 조회하는 것은 적절치 않음.
+select author_id from post group by author_id;
+
+-- group by 와 집계함수 같이 사용하는 케이스가 많음.
+-- 아래 쿼리에서 *은 그룹화된 데이터내에서의 개수
+select author_id, count(*) from post group by author_id;
+select author_id, count(*), sum(price) from post group by author_id;
+
+-- author의 email과 author 별로 본인이 쓴 글의 개수를 출력
+select a.email, (select count(*) 
+                from post 
+                where author_id=a.id) 
+from author a;
+
+select a.email
+from author a left join post p
+on a.id = p.author_id
+where a.id = p.author
+group by a.email;
+
+-- where 와 group by
+-- 연도별 post 글의 개수 출력, 연도가 null 인 값은 제외
+select date_format(create_time, '%Y'), count(*)
+from post
+where create_time in not null
+group by create_time;
+
+-- having : group by 를 통해 나온 집계값의 대한 조건
+-- 글을 2개 이상 쓴사람에 대한 정보조회
+select author_id from post group by author_id having count(*)>=2
+select author_id count(*) as count from post group by author_id having count>=2;
+
